796de223c835ec0545a29ad0c10b66b53307d23f
story_test <othmane.hachmi.23@gmail.com>
2024-07-22 19:32:44 +0100

    Add categories of the story

--- test_story.py
+++ test_story.py
@@ -5,9 +5,10 @@ def test():
     text_generator = StoryGenerator()
     category = input("Choose a catogory for the story")
     user_prompt = input("Write a description of the story")
-    story_text = text_generator.generate_story_text(user_prompt, category, length=450)
-    return story_text
+    story_text_paragraphs = text_generator.generate_story_text(user_prompt, category, length=450)
+    return story_text_paragraphs
 
 if __name__ == "__main__":
-    story_text = test()
-    print(story_text)
+    story_text_paragraphs = test()
+    for paragraph in story_text_paragraphs:
+        print(paragraph)
