a9fd551083d70db2a8f709428457dd01e4b3b989
story_test <othmane.hachmi.23@gmail.com>
2024-07-24 16:23:47 +0100

    Add the moral option

--- test_story.py
+++ test_story.py
@@ -3,9 +3,10 @@ from story_generator import StoryGenerator
 
 def test_story_text():
     text_generator = StoryGenerator()
-    category = input("Choose a catogory for the story")
-    user_prompt = input("Write a description of the story")
-    story_text_paragraphs = text_generator.generate_story_text(user_prompt, category, length=450)
+    category = input("Choose a catogory for the story: ")
+    user_prompt = input("Write a description of the story: ")
+    moral_prompt = input("Enter a moral: ")
+    story_text_paragraphs = text_generator.generate_story_text(user_prompt, category, length=450, moral=moral_prompt)
     return story_text_paragraphs
 
 if __name__ == "__main__":
